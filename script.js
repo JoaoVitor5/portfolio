e6ed4081b7cba8606902aa02d8d864e3006d6034
João <deandejoao5@gmail.com>
2026-03-20 01:57:07 -0300

    Portfólio finalizado com ajustes de espaçamento, mais textos, mais imagens e mais efeitos.

--- script.js
+++ script.js
@@ -1,58 +1,75 @@
 console.log("Meu Portfólio está interativo!");
 
-const botao = document.querySelector("#btnProjeto");
+
+//conecta as constantes lá do HTML ao JavaScript aqui, nesse caso são os botões, as imagens e o texto extra na parte 'sobre mim'.
+const botao1 = document.querySelector("#btnSobre");
+const txtsobre = document.querySelector("#maissobre");
+const botao2 = document.querySelector("#btnProjeto");
 const img1 = document.querySelector("#img1");
 const img2 = document.querySelector("#img2");
 const img3 = document.querySelector("#img3");
+const img4 = document.querySelector("#img4");
 
-let visivel = false; // controla se está mostrando ou não
+let visivelSobre = false; // faz o controle da visibilidade das imagens e textos da parte 'sobre mim'
+let visivelProjeto = false; // faz o controle da visibilidade das imagens da parte 'projeto'
 
-botao.addEventListener("click", () => {
+//addEventListener("click", () =>   isso faz com que ao clicar no determinado botão ele fará o que foi programado a fazer, neste caso esconder e mostrar o texto.
+botao1.addEventListener("click", () => {
+   if (!visivelSobre) {
+      //mostrar
+      txtsobre.style.display = "flex";
 
-   if (!visivel) {
+      setTimeout(() => {
+         txtsobre.style.opacity = "1";
+      }, 100);
+
+      visivelSobre = true;
+
+   } else {
+      //esconder
+       txtsobre.style.opacity = "0";
+       setTimeout(() => {
+                txtsobre.style.display = "none";
+       }, 100);
+
+       visivelSobre = false;
+   }
+
+});
+
+//addEventListener("click", () =>   isso faz com que ao clicar no determinado botão ele fará o que foi programado a fazer, neste caso esconder e mostrar as imagens.
+botao2.addEventListener("click", () => {
+
+   if (!visivelProjeto) {
       //MOSTRAR
       img1.style.display = "inline-block";
       img2.style.display = "inline-block";
       img3.style.display = "inline-block";
+      img4.style.display = "inline-block";
 
       setTimeout(() => {
          img1.style.opacity = "1";
          img2.style.opacity = "1";
          img3.style.opacity = "1";
+         img4.style.opacity = "1";
       }, 100);
 
-      visivel = true;
+      visivelProjeto = true;
 
    } else {
       //ESCONDER
          img1.style.opacity = "0";
          img2.style.opacity = "0";
          img3.style.opacity = "0";
+         img4.style.opacity = "0";
          setTimeout(() => {
             img1.style.display = "none";
             img2.style.display = "none";
             img3.style.display = "none";
-        }, 100); // tempo igual ao transition
+            img4.style.display = "none";
+        }, 100); // tempo igual ao de transição
 
-        visivel = false;
+        visivelProjeto = false;
    }
 
 });
-/*
-    foto2.style.display = "inline-block";
-    foto2.style.width ="50px";
-    foto2.style.height ="50px";
-    foto2.style.objectFit = "cover";
-    setTimeout(() => {
-       foto2.style.opacity = "1"; 
-    }, 100);
-
-    foto.style.display = "inline-block";
-    foto.style.width ="50px";
-    foto.style.height ="50px";
-    foto.style.objectFit = "cover";
-    setTimeout(() => {
-       foto.style.opacity = "1"; 
-    }, 100);
-});
-*/
